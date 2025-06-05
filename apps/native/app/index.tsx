3d15f9c36a378972b1474996516120f8bd728fee
Matsuyama Mairu <maturu@lambdascript.dev>
2025-06-05 23:32:16 +0900

    chore: remove link to be not founded

--- apps/native/app/index.tsx
+++ apps/native/app/index.tsx
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Link } from 'expo-router';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-      <Link href="/test">go to test</Link>
     </View>
   );
 }
