0b1d370324ea0d0c84fa276d3f076eb06dca4ae3
Matsuyama Mairu <maturu@lambdascript.dev>
2025-06-16 17:58:05 +0900

    refactor(api): add trpc error and fix health response

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,10 +1,11 @@
+import { TRPCError } from '@trpc/server'
 import { publicProcedure, router } from './lib/trpc'
 import { createUserRepository, createUserService, UserSchema } from '@repo/model'
 
 export const appRouter = router({
-  health: publicProcedure.query((args) => {
+  health: publicProcedure.query(() => {
     console.log('health check')
-    return args.signal
+    return { ok: true }
   }),
   userById: publicProcedure
     .input(UserSchema.pick({ id: true }))
@@ -14,11 +15,10 @@ export const appRouter = router({
         const userRepository = createUserRepository()
         const userService = createUserService(userRepository)
         const user = await userService.get(input.id)
-        console.log(user)
         return user
       } catch (error) {
         console.log(error)
-        return null
+        throw new TRPCError({ code: 'NOT_FOUND', cause: error })
       }
     }),
 })
