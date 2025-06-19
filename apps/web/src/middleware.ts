0b144a7d2b791ce541954484d35f06cf557870fb
Matsuyama Mairu <maturu@lambdascript.dev>
2025-06-19 14:54:00 +0900

    chore: apply formatter transform

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,6 +1,1 @@
-import { auth as middleware, NextAuthResult } from '@/lib/auth'
-import { NextResponse } from 'next/server'
-
-export default middleware((req) => {
-  return NextResponse.next()
-}) as unknown as NextAuthResult['auth']
+export { auth as middleware } from '@/lib/auth'
