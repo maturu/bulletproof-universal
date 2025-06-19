b6ed66c6e51fb2db5e20dd5effa0a8c81f3a1a96
Matsuyama Mairu <maturu@lambdascript.dev>
2025-06-19 17:37:36 +0900

    style(user-api): create instance for user repository and service in module level

--- packages/api/src/routes/user/route.ts
+++ packages/api/src/routes/user/route.ts
@@ -2,13 +2,14 @@ import { UserSchema, createUserRepository, createUserService } from '@repo/model
 import { TRPCError } from '@trpc/server'
 import { publicProcedure, router } from '../../lib/trpc'
 
+const userRepository = createUserRepository()
+const userService = createUserService(userRepository)
+
 const findById = publicProcedure
   .input(UserSchema.pick({ id: true }))
   .output(UserSchema.nullable())
   .query(async ({ input }) => {
     try {
-      const userRepository = createUserRepository()
-      const userService = createUserService(userRepository)
       const user = await userService.get(input.id)
       return user
     } catch (error) {
