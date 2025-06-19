8cc242d384460df7ee062e808cd8b6d1cbdda360
Matsuyama Mairu <maturu@lambdascript.dev>
2025-06-19 13:22:19 +0900

    feat(api): add create session and apply nested routes

---
+++ packages/api/src/routes/user/route.ts
@@ -0,0 +1,22 @@
+import { UserSchema, createUserRepository, createUserService } from '@repo/model'
+import { TRPCError } from '@trpc/server'
+import { publicProcedure, router } from '../../lib/trpc'
+
+const findById = publicProcedure
+  .input(UserSchema.pick({ id: true }))
+  .output(UserSchema.nullable())
+  .query(async ({ input }) => {
+    try {
+      const userRepository = createUserRepository()
+      const userService = createUserService(userRepository)
+      const user = await userService.get(input.id)
+      return user
+    } catch (error) {
+      console.log(error)
+      throw new TRPCError({ code: 'NOT_FOUND', cause: error })
+    }
+  })
+
+export const userRouter = router({
+  findById,
+})
